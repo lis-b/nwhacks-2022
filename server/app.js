520fa81291e9138ff8781ea745a2df3aaa9a4d29
DFriend01 <devon.fri.2000@gmail.com>
2022-01-15 13:30:34 -0800

    Removed error handlers to avoid server errors

--- server/app.js
+++ server/app.js
@@ -1,35 +1,18 @@
 const express = require('express');
+const path = require("path");
 
 // Require any routes here...
 
 ///////////////////////////////
 
-
+const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Write any app.use statements for any required routers...
 
-///////////////////////////////
-
-
-// Error handlers
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-  
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+// TESTING
+app.get('/', function(req, res) {
+    return res.send("Hello World!");
 });
 
 ///////////////////////////////
