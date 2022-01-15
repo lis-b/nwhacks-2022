f15cbebf11a1b4de8d83678d948735786d74aff2
Phil <phillipdumitru71@gmail.com>
2022-01-15 14:39:09 -0800

    updated virtual types for difficulty and time

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+const Constants = require('../constants.js');
+
 var RecipeSchema = new Schema(
     {
         name: {type:String, required: true},
@@ -11,32 +13,14 @@ var RecipeSchema = new Schema(
     }
 );
 
-RecipeSchema.virtual('difficulty_tostring').get(function() {
-    switch(this.difficulty){
-        case 0:
-            return 'beginner'
-        case 1:
-            return 'intermediate'
-        case 2:
-            return 'advanced'
-        default:
-            return 'error?'; //TODO: figure out how to error this in js?
-    }
+//difficulty string virtual type
+RecipeSchema.virtual('difficultyString').get(function() {
+    return Constants.difficulties[this.difficulty]; 
 });
 
-RecipeSchema.virtual('time_tostring').get(function() {
-    switch(this.time){
-        case 0:
-            return 'less 30 minutes';
-        case 1:
-            return 'under 1 hour';
-        case 2:
-            return '1 - 2 hours';
-        case 3:
-            return '2 - 3 hours';
-        case 4:
-            return 'more than 4 hours'
-    }
+//time string virtual type
+RecipeSchema.virtual('timeString').get(function() {
+    return Constants.times[this.time];
 });
 
 module.exports = mongoose.model('Recipe', RecipeSchema);
