eefd1ce95a1451c9cf8f1e798256131a10e82d7a
Phil <phillipdumitru71@gmail.com>
2022-01-15 19:57:15 -0800

    template routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('api/search_view', function(req,res,next){
+    //TODO
+});
 
+router.get('api/recipe', function(req,res,next){
+    //TODO
+})
 
 module.exports = router;
